7a3ae66bad7e7d380194f13ea4743235d9c877eb
Kristen Shea <kstar61@live.com>
2018-09-03 21:04:43 -0500

    *

--- individual characters from string/Program.cs
+++ individual characters from string/Program.cs
@@ -7,14 +7,19 @@ namespace individual_characters_from_string
         static void Main(string[] args)
         {
             Console.WriteLine("Type a short sentence, quote, or string of gibberish: ");
-            string quote = Console.ReadLine().ToUpper();
+            string quote = Console.ReadLine().ToUpper().Trim();
+            //set quote as a string, converted it to all caps, trimmed extra spaces at the beginning and end
+            //so the extra spaces wouldn't print to console or be counted in string length.
             Console.WriteLine();
 
+            //for each loop to print each character in user-entered sentence on a line by itself.
             foreach (char c in quote)
             {
                 Console.WriteLine(c.ToString().ToUpper());
             }
             Console.WriteLine();
+
+            //to figure out the length of the quote and then print it to console.
             int quoteLength = quote.Length;
 
             Console.WriteLine("The lenght of the quote is: {0} characters", quoteLength);
