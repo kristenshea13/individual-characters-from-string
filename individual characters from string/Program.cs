4bf686111ba408668e861fd0d9e782f2c558a840
Kristen Shea <kstar61@live.com>
2018-09-03 20:32:45 -0500

    Add project files.

---
+++ individual characters from string/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace individual_characters_from_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
